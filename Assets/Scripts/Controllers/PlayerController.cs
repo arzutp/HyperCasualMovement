fccd6498fb3d165c5076241069e5fcd01926c239
arzutepe <arzu.tepe.99@gmail.com>
2022-10-27 14:45:33 +0300

    player movement

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float playerSpeed = 5f;
+    [SerializeField] PlayerModel playerModel;
+    [SerializeField] Rigidbody rb;
+    Vector3 direction;
+    float force = 25f;
+
+    public void Initialie()
+    {
+        transform.position = Vector3.zero;
+        playerModel.SetAnimReset();
+    }
+
+    private void Update()
+    {
+        playerMovement();
+    }
+
+    #region movement
+    private void playerMovement()
+    {
+            if (Input.GetMouseButton(0))  //hareket ?slemleri
+            {
+                Plane plane = new Plane(Vector3.up, transform.position);
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+                if (plane.Raycast(ray, out var distance))
+                    direction = ray.GetPoint(distance);
+
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(direction.x, 0f, direction.z), playerSpeed * Time.deltaTime);
+                transform.LookAt(direction);
+                Vector3 offset = direction - transform.position;
+                if (offset.magnitude > 1f)
+                    transform.LookAt(direction);
+            }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    public void SetAttack(bool isRun)
+    {
+        //animator.SetBool("Run",isRun);
+    }
+
+    public void SetAnimReset()
+    {
+        animator.Rebind();
+    }
+}
